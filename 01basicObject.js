fa0fad998ce16e213669ffcbd8400b9865c740c9
shon <shonbwd@gmail.com>
2024-01-03 11:11:50 +0530

    first commit

---
+++ 01basicObject.js
@@ -0,0 +1,8 @@
+//* Create a Basic JavaScript Object
+
+//? Create a dog object with name and numLegs properties, and set them to a string and a number, respectively.
+
+let dog = {
+  name: "Aflac",
+  numLegs: 4,
+};
---
+++ 02_dotNotation.js
@@ -0,0 +1,3 @@
+//* Use Dot Notation to Access the Properties of an Object
+
+//? Print both properties of the dog object to your console.
