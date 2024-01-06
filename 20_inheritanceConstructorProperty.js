d0687b7f6ab764c4178f988acce9dc604e62bd89
shon <shonbwd@gmail.com>
2024-01-06 15:04:42 +0530

    completed more tasks

---
+++ 20_inheritanceConstructorProperty.js
@@ -0,0 +1,17 @@
+//* Reset an Inherited Constructor Property
+
+//? Fix the code so duck.constructor and beagle.constructor return their respective constructors.
+
+function Animal() {}
+function Bird() {}
+function Dog() {}
+
+Bird.prototype = Object.create(Animal.prototype);
+Dog.prototype = Object.create(Animal.prototype);
+
+// Only change code below this line
+Bird.prototype.constructor = Bird;
+Dog.prototype.constructor = Dog;
+
+let duck = new Bird();
+let beagle = new Dog();
